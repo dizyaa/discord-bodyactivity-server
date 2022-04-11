1d554158b1c8010586bd46a21214afe20d3f4263
DizelBadCoder <diskroma69@gmail.com>
2022-04-11 20:23:07 +0400

    add: init first report

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import asyncio
+import crud
+import schemas
 import discord_integration
 from hypercorn.asyncio import serve
 from hypercorn.config import Config
+from database import SessionLocal
 
 import server
 
-if __name__ == '__main__':
-    discord_integration.start()
 
+def init_first_report():
+    last_report = crud.get_last_report(SessionLocal())
+
+    report = schemas.ReportCreate(
+        heartRate=last_report.heartRate,
+        steps=last_report.steps,
+        activity=last_report.activity,
+        time=last_report.time,
+    )
+
+    discord_integration.update(report=report)
+
+
+def run_server():
     config = Config()
     config.bind = ["0.0.0.0:8000"]
+
     asyncio.run(serve(server.app, config))
+
+
+if __name__ == '__main__':
+    discord_integration.start()
+    init_first_report()
+    run_server()
